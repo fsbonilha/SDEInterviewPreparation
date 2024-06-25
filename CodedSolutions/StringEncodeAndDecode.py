c5e0cf85ce56e7f0e5dd3d028bf6c61a1d54a515
Felipe Bonilha <bonilhfe@amazon.com>
2024-06-25 14:27:57 -0300

    add StringEncodeAndDecode solution

---
+++ CodedSolutions/StringEncodeAndDecode.py
@@ -0,0 +1,31 @@
+# Source: https://neetcode.io/problems/string-encode-and-decode 
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        joined = ""
+        for currentString in strs:
+            joined = joined + str(len(currentString)) + "$" + currentString
+        return joined
+
+    def decode(self, s: str) -> List[str]:
+        stringList = []
+        index = 0
+
+        while index < len(s):
+            currentString = ""
+            sizeString = ""
+
+            while s[index] != "$":
+                sizeString += s[index]
+                index += 1
+            index += 1
+
+            sizeStringInt = int(sizeString)
+            while sizeStringInt > 0:
+                currentString += s[index]
+                sizeStringInt -= 1
+                index += 1
+            stringList.append(currentString)
+
+        return stringList
