0243861b55b5ff58ee9e0c007b2651c7fd598910
Felipe Bonilha <bonilhfe@amazon.com>
2024-06-25 19:14:31 -0300

    add ValidSudoku solution

---
+++ AmazonInterviewList/09.ValidSudoku.py
@@ -0,0 +1,26 @@
+# Source: 
+# https://leetcode.com/problems/valid-sudoku/
+# https://neetcode.io/problems/valid-sudoku
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        linSize = len(board)
+        colSize = len(board[0])
+
+        colMaps = [set() for _ in range(colSize)]
+        linMaps = [set() for _ in range(linSize)]
+        boxMaps = [[set() for _ in range(colSize//3)] for _ in range(linSize//3)]
+        for indexLine, line in enumerate(board):
+            for indexCol, num in enumerate(line):
+                if num == ".":
+                    continue
+                indexBoxCol = indexCol//3
+                indexBoxLine = indexLine//3
+                if (num in colMaps [indexCol] or
+                    num in linMaps[indexLine] or
+                    num in boxMaps[indexBoxLine][indexBoxCol]):
+                    return False
+                colMaps[indexCol].add(num)
+                linMaps[indexLine].add(num)
+                boxMaps[indexBoxLine][indexBoxCol].add(num)
+        return True
