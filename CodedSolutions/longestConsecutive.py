7bb27c596e7abfaefd45f40d922cf4ad0f6cd84f
Felipe Bonilha <bonilhfe@amazon.com>
2024-06-25 19:37:21 -0300

    add longestConsecutive solution

---
+++ CodedSolutions/longestConsecutive.py
@@ -0,0 +1,18 @@
+# Source: https://neetcode.io/problems/longest-consecutive-sequence
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numsSet = set(nums)
+        longest = 0
+        for num in numsSet:
+            if num - 1 in numsSet:
+                continue
+
+            currentNum = num
+            size = 1
+            while currentNum + 1 in numsSet:
+                size += 1
+                currentNum += 1
+            if size > longest:
+                longest = size
+        return longest
