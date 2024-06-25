a96740ad25fa25167daeff4f62b98f48bbd1e3e7
Felipe Bonilha <bonilhfe@amazon.com>
2024-06-24 21:45:21 -0300

    add AnagramGroups and TopKElements solutions

---
+++ CodedSolutions/AnagramGroups.py
@@ -0,0 +1,20 @@
+# Source: https://neetcode.io/problems/anagram-groups
+
+# Developed Solution: NeetCode
+
+# Complexity: O(n * m)
+# n = # of words 
+# m = average size of word
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        letterCounts = dict()
+        for word in strs:
+            letterCount = [0] * 26
+            for letter in word:
+                letterCount[ord(letter) - ord('a')] += 1
+            if tuple(letterCount) in letterCounts.keys():
+                letterCounts[tuple(letterCount)].append(word)
+            else:
+                letterCounts[tuple(letterCount)] = [word]
+        return letterCounts.values()
---
+++ CodedSolutions/TopKElementsinList.py
@@ -0,0 +1,26 @@
+# Source: https://neetcode.io/problems/top-k-elements-in-list
+
+# Complexity: O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        countsMap = {}
+        for num in nums:
+            if num in countsMap.keys():
+                countsMap[num] += 1
+            else:
+                countsMap[num] = 1
+
+        counts = [[] for i in range(len(nums) + 1)]
+        for index, count in countsMap.items():
+            counts[count] += [index]
+        
+        solution = []
+        for index in range(len(counts) - 1, 0, -1):
+            for num in counts[index]:
+                solution.append(num)
+                if len(solution) == k:
+                    return solution
+
+
+            
