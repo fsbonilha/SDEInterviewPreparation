e9ae215c79c8ac8f2cd6590145d54fad2f2ba6b3
Felipe Bonilha <bonilhfe@amazon.com>
2024-06-27 23:05:23 -0300

    add BuyAndSellCrypto solution

---
+++ CodedSolutions/BuyAndSellCrypto.py
@@ -0,0 +1,22 @@
+# Problem: Buy and Sell Crypto
+
+# Source: https://neetcode.io/problems/buy-and-sell-crypto
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = left + 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+        return maxProfit
+            
