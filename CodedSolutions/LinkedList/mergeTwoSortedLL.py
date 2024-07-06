d4967e961a724f67c887a8a6c91654b961b70aeb
Felipe Bonilha <bonilhfe@amazon.com>
2024-07-05 23:23:10 -0300

    short solution for mergeTwoSortedLL

--- CodedSolutions/LinkedList/mergeTwoSortedLL.py
+++ CodedSolutions/LinkedList/mergeTwoSortedLL.py
@@ -17,43 +17,69 @@ class Solution:
                       list2: Optional[ListNode]) -> Optional[ListNode]:
         item1, item2 = list1, list2
 
-        merged = None
-        mergedHead = None
+        dummy = ListNode()
+        tail = dummy
 
         while item1 and item2:
-            if item1.val <= item2.val:
-                if merged:
-                    merged.next = item1
-                    merged = merged.next
-                else:
-                    merged = item1
-                    mergedHead = merged
+            if item1.val < item2.val:
+                tail.next = item1
                 item1 = item1.next
             else:
-                if merged:
-                    merged.next = item2
-                    merged = merged.next
-                else:
-                    merged = item2
-                    mergedHead = merged
+                tail.next = item2
                 item2 = item2.next
+            tail = tail.next
 
-        while item1:
-            if merged:
-                merged.next = item1
-                merged = merged.next
-            else:
-                merged = item1
-                mergedHead = merged
-            item1 = item1.next
+        if item1:
+            tail.next = item1
+        elif item2:
+            tail.next = item2
 
-        while item2:
-            if merged:
-                merged.next = item2
-                merged = merged.next
-            else:
-                merged = item2
-                mergedHead = merged
-            item2 = item2.next
+        return dummy.next
 
-        return mergedHead
+# First Solution (Long):
+#            
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode],
+#                       list2: Optional[ListNode]) -> Optional[ListNode]:
+#         item1, item2 = list1, list2
+
+#         merged = None
+#         mergedHead = None
+
+#         while item1 and item2:
+#             if item1.val <= item2.val:
+#                 if merged:
+#                     merged.next = item1
+#                     merged = merged.next
+#                 else:
+#                     merged = item1
+#                     mergedHead = merged
+#                 item1 = item1.next
+#             else:
+#                 if merged:
+#                     merged.next = item2
+#                     merged = merged.next
+#                 else:
+#                     merged = item2
+#                     mergedHead = merged
+#                 item2 = item2.next
+
+#         while item1:
+#             if merged:
+#                 merged.next = item1
+#                 merged = merged.next
+#             else:
+#                 merged = item1
+#                 mergedHead = merged
+#             item1 = item1.next
+
+#         while item2:
+#             if merged:
+#                 merged.next = item2
+#                 merged = merged.next
+#             else:
+#                 merged = item2
+#                 mergedHead = merged
+#             item2 = item2.next
+
+#         return mergedHead
